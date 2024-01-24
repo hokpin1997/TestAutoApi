d749ec7ff6b39bbee1aee06e71a71de6b6a7faee
haobin_su <haobin.su@weimob.com>
2024-01-24 17:16:20 +0800

    封装operation.loginPage

---
+++ page/Login/loginPage.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+from utils.otherUtils.read_data import GetYamlData, ensure_path_sep
+from utils.requestsUtils.requestControl import RequestControl
+
+
+config = GetYamlData(ensure_path_sep("common/conf.yaml")).get_yaml_data()
+api_test_url = config["host"]["api_test"]
+
+
+class LoginPage(RequestControl):
+
+    def password_login(self, **kwargs):
+        return self.post("/user/login", **kwargs)
+
+    def verification_code(self):
+        pass
+
+    def forget_password(self):
+        pass
+
+
+loginPage = LoginPage(api_test_url)
+
+
+if __name__ == '__main__':
+    pass
